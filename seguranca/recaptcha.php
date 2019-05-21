02707630fe9e9f645ac2c88635d3748a5373eb65
uHs13 <heitorhenriquedias@gmail.com>
2019-05-21 08:14:59 -0300

    082021052019

--- seguranca/recaptcha.php
+++ seguranca/recaptcha.php
@@ -1,7 +1,26 @@
 <?php 
 
-	
+
 
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Recaptcha</title>
+	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+<body>
+	<main>
+		
+		<form action='cadastro.php' method='POST'>
+			<div class="g-recaptcha" data-sitekey="6LcLqqQUAAAAAFsEe31K58knEBVCzTAHqVx8xhZR"></div>
+			<input type="text" name="email">
+			<button>Enviar</button>
+
+		</form>
+
+	</main>
+</body>
+</html>
