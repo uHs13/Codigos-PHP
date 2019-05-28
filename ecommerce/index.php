50a979a46cb7ee5af50fe2b89640fbc296dd50fe
uHs13 <heitorhenriquedias@gmail.com>
2019-05-28 08:16:08 -0300

    082028052019

---
+++ ecommerce/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+require_once("vendor/autoload.php");//require do autoloader do composer
+
+$app = new \Slim\Slim();//instancia uma nova aplicação slim
+
+$app->config('debug', true);//configurando como true mostra o log completo do erro
+
+$app->get('/', function() {
+    
+	echo "START";
+
+});
+
+$app->run();
+
+ ?>
