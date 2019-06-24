b86166223f5802f38ea33a4b73bba4ffeffac6f8
uHs13 <heitorhenriquedias@gmail.com>
2019-06-24 08:25:13 -0300

    083024062019

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -373,6 +373,24 @@ $app->post('/admin/categories/:idcategory', function ($idcategory) {
 
 });
 
+
+$app->get("/category/:idcategory", function ($idcategory) {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl('category', [
+
+		'category' => $category->getValues(),
+		'products' => []
+
+	]);
+
+});
+
 $app->run();
 
 ?>
--- ecommerce/vendor/hcodebr/php-classes/src/Model/Category.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/Category.php
@@ -33,6 +33,8 @@ class Category extends Model
 
 		$this->setData($results[0]);
 
+		Category::updateFile();
+
 	}//save()
 
 
@@ -62,9 +64,26 @@ class Category extends Model
 
 		));
 
+		Category::updateFile();
+
 	}//delete()
 
+	public static function updateFile()
+	{
 
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			
+			array_push($html, "<li><a href='/PHP/ecommerce/category/".$row['idcategory']."'>".$row['descategory']."</a></li>");
+
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT']. DIRECTORY_SEPARATOR ."PHP" . DIRECTORY_SEPARATOR . "ecommerce". DIRECTORY_SEPARATOR ."views" . DIRECTORY_SEPARATOR . "categoriesMenu.html" , implode('', $html));
+
+	}
 
 }//User
 
